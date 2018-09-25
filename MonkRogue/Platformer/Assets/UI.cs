3dfbb18d21c53041765d2d938d4906d62f51ac55
RuneRobin <RuneRobin@users.noreply.github.com>
2018-09-25 14:05:27 +0100

    Add files via upload
    My other 'Platformer'-named project, as I wasn't aware the other project was also called Platformer, this running project derives its inspiration from risk of rain, although work on it has stopped due to complications trying to find a job to give me an income so that I may spend my free time working on it without worrying about my living condition.

---
+++ MonkRogue/Platformer/Assets/UI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+
+    public Text Mana;
+    public Text Health;
+    public Text Ability;
+    //PlatformChanger platChange;
+	// Use this for initialization
+	void Start ()
+    {
+        //platChange = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<PlatformChanger>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Health.text = "Health: " + (int)platChange.health;
+        //Mana.text = "Mana: " + (int)platChange.Mana;
+        //Ability.text = "Ability: " + platChange.currentAbility;
+	}
+}
